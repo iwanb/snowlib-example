594aa5ab110e0988de944219a323c7bf36ab11d1
ibriquem <iwan.briquemont@nokia.com>
2024-03-21 13:55:44 +0100

    Initial

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "My Nix library";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      inherit inputs;
+
+      src = ./.;
+      snowfall.root = ./nix;
+    };
+}
